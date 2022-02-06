b792536e5b0d8ca5cc0f54d0615e37aee721b812
Arya-Jain <89778433+Arya-Jain@users.noreply.github.com>
2022-02-06 11:06:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+status = "";
+
+function setup(){
+    canvas = createCanvas(380,280);
+    canvas.center();
+
+    video  = createCapture(VIDEO);
+    video.hide();
+
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modeLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+
+    object_name = document.getElementById("object_name").value;
+}
+
+function modeLoaded(){
+    console.log("model loaded!");
+    status = true;
+}
+
+function draw(){
+    image(video, 0,0, 380, 280);
+}
